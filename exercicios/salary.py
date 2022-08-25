941620b249f5086ed07d68e1d14d0e3c326fda13
Rafael Haruo <rafael.hoshino@edu.unifil.br>
2022-08-24 21:00:31 -0300

    Commit Quarta

---
+++ exercicios/salary.py
@@ -0,0 +1,10 @@
+a = int(input("Coloqu seu numero "))
+b = float(input("Coloque sua horas  "))
+c = float(input("Coloque quanto vale sua hora  "))
+
+
+salay = (b *c ) 
+
+
+print (f"Numero = {a}")
+print(f"Salay =  {salay} ")
